67bef32496d07a3f19d46eb940ca743eac90048b
theo2023 <theo2023@theo2023-pc>
2019-08-26 21:48:01 -0400

    Commented out code that printed version.

--- src/a1/A1Example.java
+++ src/a1/A1Example.java
@@ -2,14 +2,15 @@ package a1;
 
 import java.util.Scanner;
 
-public class A1Example {
-
-	public static void main(String[] args) {
+public class A1Example
+{
+	public static void main(String[] args)
+	{
 		// Print out the Java version number, just to let 
 		// you know what Eclipse thinks it is using and
 		// so that you can confirm that it is 10.0 or higher.
 		
-		System.out.println("Your Java version: " + System.getProperty("java.version"));
+		// System.out.println("Your Java version: " + System.getProperty("java.version"));
 		
 		// Set up scanner object to read space-separated values
 		// from console input
@@ -20,15 +21,14 @@ public class A1Example {
 		
 		int count = scan.nextInt();
 		
-		
 		// Create an array to store numbers.
 		
 		int[] values = new int[count];
 		
-		
 		// Read values into the array
 		
-		for (int i=0; i<values.length; i++) {
+		for (int i = 0; i < values.length; i++)
+		{
 			values[i] = scan.nextInt();
 		}
 		
@@ -46,7 +46,6 @@ public class A1Example {
 		System.out.println("Min: " + min);
 		System.out.println("Max: " + max);
 		System.out.println("Average: " + String.format("%.2f", ((double) sum) / ((double) count)));
-		
 	}
 	
 	/* calculateValueSum 
@@ -60,14 +59,14 @@ public class A1Example {
 	 * Input array must not be null.
 	 */
 	
-	static int calculateValueSum(int[] vals) {
-		
+	static int calculateValueSum(int[] vals)
+	{
 		int sum = 0;
 		
-		for (int i=0; i<vals.length; i++) {
+		for (int i = 0; i < vals.length; i++)
+		{
 			sum += vals[i];
 		}
-		
 		return sum;
 	}
 	
@@ -82,20 +81,21 @@ public class A1Example {
 	 * Input array must not be null and must contain at least one value.
 	 */
 	
-	static int findValueMin(int[] vals) {
-		
+	static int findValueMin(int[] vals)
+	{
 		// Initialize current minimum to first value in array.
 		int cur_min = vals[0];
 		
 		// Starting with second value (if any), compare each value
 		// in array with current minimum and replace if smaller.
 		
-		for (int i=1; i < vals.length; i++) {
-			if (vals[i] < cur_min) {
+		for (int i = 1; i < vals.length; i++)
+		{
+			if (vals[i] < cur_min)
+			{
 				cur_min = vals[i];
 			}
 		}
-		
 		return cur_min;
 	}
 
@@ -110,21 +110,21 @@ public class A1Example {
 	 * Input array must not be null and must contain at least one value.
 	 */
 	
-	static int findValueMax(int[] vals) {
-		
+	static int findValueMax(int[] vals)
+	{
 		// Initialize current minimum to first value in array.
 		int cur_max = vals[0];
 		
 		// Starting with second value (if any), compare each value
 		// in array with current minimum and replace if smaller.
 		
-		for (int i=1; i < vals.length; i++) {
-			if (vals[i] > cur_max) {
+		for (int i = 1; i < vals.length; i++)
+		{
+			if (vals[i] > cur_max)
+			{
 				cur_max = vals[i];
 			}
 		}
-		
 		return cur_max;
 	}
-
 }
